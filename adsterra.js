812b9d8c3086e80eb5982f882b6af3f75a178db6
meherul382 <meherulhassan62@gmail.com>
2026-09-02 05:58:44 +0600

    Remove Adsterra ad network before AdSense application

--- adsterra.js
+++
@@ -1,8 +0,0 @@
-/* EarnBD Adsterra loader. Keep the publisher script unchanged and load it once per page. */
-(function () {
-  if (document.querySelector('script[data-earn-bd-adsterra]')) return;
-  var s = document.createElement('script');
-  s.src = 'https://movementssubscriptionobjection.com/bd/69/05/bd6905d20fbe6ac804838a3fa363f603.js';
-  s.setAttribute('data-earn-bd-adsterra', 'true');
-  document.body.appendChild(s);
-})();
